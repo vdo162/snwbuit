3424c1b5718823a2197bd3fa6abeea2f8b873fcf
vdo162 <vdo162@gmail.com>
2021-07-24 11:22:46 +0300

    fixed rerenderEntireTree

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import {rerenderEntireTree} from './render';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
 import {state} from './redux/state.js';
 import {addPost} from './redux/state.js';
 
-rerenderEntireTree();
+rerenderEntireTree(state, addPost);
 reportWebVitals();
 
 // If you want to start measuring performance in your app, pass a function
