bbb3c4b3d69c3b37183f042bc641086c63f20cc5
vdo162 <vdo162@gmail.com>
2021-07-24 08:50:39 +0300

    added addPost-handler

--- src/components/Main/Profile/MyPosts/MyPosts.jsx
+++ src/components/Main/Profile/MyPosts/MyPosts.jsx
@@ -1,17 +1,22 @@
+import React from 'react';
 import s from './MyPosts.module.css';
 import {Post} from './Post/Post.jsx';
 
 export const MyPosts = (props) => {
 	let postsElement = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount} key={p.id}/>);
+	let post = React.createRef();
+	let addPost = () => {
+		alert(post.current.value);
+	}
 	return (
 		<div className={s.postBlock}>
 			<h3>My posts</h3>
 			<div>
 				<div> 
-					<textarea></textarea>
+					<textarea ref={post}></textarea>
 				</div>
 				<div>
-					<button>Add post</button>
+					<button onClick={addPost}>Add post</button>
 				</div>
 			</div>
 			<div  className={s.posts}>
