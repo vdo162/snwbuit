5e1b00bc99af872e011a43c72b39fce6f2919325
vdo162 <vdo162@gmail.com>
2021-07-24 20:18:38 +0300

    35 created the subscribe of the state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import {rerenderEntireTree} from './render';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
 import reportWebVitals from './reportWebVitals';
-import {state} from './redux/state.js';
+import {BrowserRouter} from 'react-router-dom';
+import App from './App';
+import {subscribe, state, addPost, updateNewPostText} from './redux/state.js';
 
+export let rerenderEntireTree = (state) => {
+	ReactDOM.render(
+	  <React.StrictMode>
+		<BrowserRouter>
+			<App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+		</BrowserRouter>
+	  </React.StrictMode>,
+	  document.getElementById('root')
+	);
+}
 rerenderEntireTree(state);
+subscribe(rerenderEntireTree);
 reportWebVitals();
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from '../render.js';
+let rerenderEntireTree = () => {
+	console.log('State changed')
+};
 
 export let state = {
 	profilePage:{
@@ -31,7 +33,7 @@ export let state = {
 
 window.state = state;
 
-export let addPost = () => {
+export const addPost = () => {
 	let newPost = {
 		id: 5, 
 		message: state.profilePage.newPostText, 
@@ -43,7 +45,11 @@ export let addPost = () => {
 	rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
 	state.profilePage.newPostText = newText;
 	rerenderEntireTree(state);
 }
+
+export const subscribe = (observer) => {
+	rerenderEntireTree = observer;
+};
