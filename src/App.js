1083ff90d191d9a882bf7753d745b0d254fa150b
vdo162 <vdo162@gmail.com>
2021-07-24 08:02:57 +0300

    30 replaced BrouzerRouter from App to index.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import {Header} from './components/Header/Header.jsx';
 import {Navbar} from './components/Navbar/Navbar.jsx';
 import {Main} from './components/Main/Main.jsx';
 import {StatusFriends} from './components/StatusFriends/StatusFriends.jsx';
-import {BrowserRouter} from 'react-router-dom';
 
 const App = (props) => {
   return (
-	<BrowserRouter>
-		<div className='appWrapper'>
-			<Header />
-			<Navbar />
-			<Main state={props.state}/>
-			<StatusFriends />		
-		</div>
-	</BrowserRouter>
+	<div className='appWrapper'>
+		<Header />
+		<Navbar />
+		<Main state={props.state}/>
+		<StatusFriends />		
+	</div>
   );
 }
 
