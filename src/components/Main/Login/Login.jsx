f0d279a293e56907e56b4e8bbb99129a388673ad
vdo162 <vdo162@gmail.com>
2021-08-21 10:24:18 +0300

    75 changed Login with Final Form

--- src/components/Main/Login/Login.jsx
+++ src/components/Main/Login/Login.jsx
@@ -1,19 +1,35 @@
+import { Form, Field } from "react-final-form";
 import s from './Login.module.css';
 
-const Login = () => {
-  return (
-	<div className={s.login}>
-		Write your login and password
-		<div className={s.login}>
-			<input placeholder='login'/>
-		</div>
-		<div className={s.login}>
-			<input placeholder='password'/>
-		</div>
-		<div className={s.login}>
-			<button>Log in</button>
-		</div>
-	</div>
-  );
+const Login = (props) => {
+	const onSubmit = (value) => {
+		console.log(value);
+	}
+	
+	return (
+		<Form 
+			onSubmit={onSubmit}
+			render={({ handleSubmit, form, submitting, pristine, values, ...props }) =>{
+				debugger
+				return (
+					<form name='auth' className={s.form} onSubmit={handleSubmit}>
+						Write your login and password
+						<div>
+							<Field name='login' component='input'  placeholder='Email'/>
+						</div>
+						<div>
+							<Field name='password' component='input' type='password' placeholder='Password'/>
+						</div>
+						<div>
+							<Field name='rememberMe' component='input' type='checkbox'/> remember me
+						</div>
+						<div>
+							<button>Login</button>
+						</div>
+					</form>
+				) 
+			 }}
+		/>
+	);
 }
 export default Login;
